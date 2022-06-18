4efc5b92348fe7506490bd471a779a38dd9d71a5
Mohamed Medhat <mmedhat@instabug.com>
2022-06-18 20:16:48 +0200

    - Added the base initialization for the project

--- app/src/main/java/com/mohamed/medhat/calendarevents/ui/main/MainActivity.kt
+++ app/src/main/java/com/mohamed/medhat/calendarevents/ui/main/MainActivity.kt
@@ -1,8 +1,14 @@
-package com.mohamed.medhat.calendarevents
+package com.mohamed.medhat.calendarevents.ui.main
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.mohamed.medhat.calendarevents.R
+import dagger.hilt.android.AndroidEntryPoint
 
+/**
+ * Represents the main screen of the app that holds the fragments of the app.
+ */
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
---
+++ app/src/main/java/com/mohamed/medhat/calendarevents/ui/main/MainFragment.kt
@@ -0,0 +1,11 @@
+package com.mohamed.medhat.calendarevents.ui.main
+
+import androidx.fragment.app.Fragment
+import com.mohamed.medhat.calendarevents.R
+import dagger.hilt.android.AndroidEntryPoint
+
+/**
+ * Represents the screen where the user can create/delete calendar events.
+ */
+@AndroidEntryPoint
+class MainFragment : Fragment(R.layout.fragment_main)
